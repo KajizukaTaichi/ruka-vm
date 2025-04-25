059c8c18f097f4cded9f73c2d55b8c9cb7a31f4f
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 22:51:24 +0900

    Update asm.rs

--- src/asm.rs
+++ src/asm.rs
@@ -1,58 +1,66 @@
 use crate::*;
+use std::collections::HashMap;
+
+type Labels = HashMap<String, f64>;
 
 pub fn asm(source: &str) -> Option<Vec<Instruction>> {
     let mut instructions = Vec::new();
+    let mut labels = Labels::new();
+    let mut index = 0;
     for line in source.lines() {
         let (line, _comment) = line.split_once(';').unwrap_or((line, ""));
-        if let Some(mnemonic) = Instruction::asm(line) {
+        if let Some(mnemonic) = Instruction::asm(line.trim(), &mut labels) {
             instructions.push(mnemonic);
+            index += 1;
+        } else {
+            labels.insert(line.trim().to_string(), index as f64);
         }
     }
     Some(instructions)
 }
 
 impl Instruction {
-    fn asm(source: &str) -> Option<Self> {
+    fn asm(source: &str, labels: &mut Labels) -> Option<Self> {
         let (opecode, operands) = source.split_once(' ').unwrap_or((source, ""));
         let operands = operands.split(',').collect::<Vec<_>>();
         Some(match opecode {
             "mov" => Self::Mov(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "add" => Self::Add(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "mul" => Self::Mul(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "neg" => Self::Neg(Register::asm(operands.get(0)?.trim())?),
             "inv" => Self::Inv(Register::asm(operands.get(0)?.trim())?),
             "eql" => Self::Eql(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "les" => Self::Les(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "jmp" => Self::Jmp(
-                Operand::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(0)?.trim(), labels)?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
-            "cal" => Self::Cal(Operand::asm(operands.get(0)?.trim())?),
+            "cal" => Self::Cal(Operand::asm(operands.get(0)?.trim(), labels)?),
             "ret" => Self::Ret,
             "lda" => Self::Lda(
                 Register::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
             "sta" => Self::Sta(
-                Operand::asm(operands.get(0)?.trim())?,
-                Operand::asm(operands.get(1)?.trim())?,
+                Operand::asm(operands.get(0)?.trim(), labels)?,
+                Operand::asm(operands.get(1)?.trim(), labels)?,
             ),
-            "psh" => Self::Psh(Operand::asm(operands.get(0)?.trim())?),
+            "psh" => Self::Psh(Operand::asm(operands.get(0)?.trim(), labels)?),
             "pop" => Self::Pop(Register::asm(operands.get(0)?.trim())?),
             "nop" => Self::Nop,
             "hlt" => Self::Hlt,
@@ -62,11 +70,13 @@ impl Instruction {
 }
 
 impl Operand {
-    fn asm(source: &str) -> Option<Self> {
+    fn asm(source: &str, labels: &mut Labels) -> Option<Self> {
         Some(if let Some(register) = Register::asm(source) {
             Self::Register(register)
         } else if let Ok(literal) = source.parse() {
             Self::Literal(literal)
+        } else if let Some(literal) = labels.get(source) {
+            Self::Literal(*literal)
         } else {
             return None;
         })
