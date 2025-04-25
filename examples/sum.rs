29585fcd00c0a84ebe94434a4d26a6223a48c6ef
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 22:47:19 +0900

    Update sum.rs

--- examples/sum.rs
+++ examples/sum.rs
@@ -2,6 +2,7 @@ use ruka_vm::*;
 
 fn main() {
     let bytecode = asm(include_str!("sum.asm").trim()).unwrap();
+    dbg!(&bytecode);
     let mut vm = RukaVM::new(bytecode);
     vm.run().unwrap();
     vm.dump();
