ccfef5f853730172b81fa9ac55a9539f0458ac94
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 22:43:48 +0900

    Update sum.rs

--- examples/sum.rs
+++ examples/sum.rs
@@ -1,22 +1,8 @@
 use ruka_vm::*;
 
 fn main() {
-    let mut vm = RukaVM::new(vec![
-        // スタックにデータを積む
-        Instruction::Psh(Operand::Literal(10.0)), // 0
-        Instruction::Psh(Operand::Literal(3.0)),  // 1
-        Instruction::Psh(Operand::Literal(5.0)),  // 2
-        // 関数呼び出し
-        Instruction::Cal(Operand::Literal(6.0)), // 3
-        Instruction::Sta(Operand::Literal(0.0), Operand::Register(Register::Rax)), // 4
-        Instruction::Hlt,                        // 5
-        // スタックにある値を全部足す関数
-        Instruction::Pop(Register::Rdx), // 6
-        Instruction::Add(Register::Rax, Operand::Register(Register::Rdx)), // 7
-        // スタックに値がある限り繰り返す
-        Instruction::Jmp(Operand::Register(Register::Rsp), Operand::Literal(6.0)), // 8
-        Instruction::Ret,                                                          // 9
-    ]);
-    vm.run();
+    let bytecode = asm(include_str!("sum.asm").trim()).unwrap();
+    let mut vm = RukaVM::new(bytecode);
+    vm.run().unwrap();
     vm.dump();
 }
