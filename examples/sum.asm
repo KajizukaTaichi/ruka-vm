28e5668e9ea69d95f9cb878811f70226a70f4df1
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 23:00:48 +0900

    Update sum.asm

--- examples/sum.asm
+++ examples/sum.asm
@@ -1,13 +1,15 @@
 main:
-    psh 1
+    psh 1   ; スタックに値を積む
     psh 2
     psh 3
-    cal sum
-    sta 0, rax
-    hlt
+    cal sum     ; 合計する
+    sta 0, rax  ; 結果をメモリに保存
+    hlt         ; プログラムを終了する
 
+
+; スタックにある値を合計する関数
 sum:
-    pop rdx
-    add rax, rdx
-    jmp rsp, sum
+    pop rdx         ; スタックから値を取り出す
+    add rax, rdx    ; 加算する (合計値はraxに格納)
+    jmp rsp, sum    ; スタックが空になるまで繰り返す
     ret
