f22c702a4be37db0b4b550b9465ae2e493f445dd
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-25 22:19:58 +0900

    Update sum.rs

--- examples/sum.rs
+++ examples/sum.rs
@@ -7,14 +7,15 @@ fn main() {
         Instruction::Psh(Operand::Literal(3.0)),  // 1
         Instruction::Psh(Operand::Literal(5.0)),  // 2
         // 関数呼び出し
-        Instruction::Cal(Operand::Literal(5.0)), // 3
-        Instruction::Hlt,                        // 4
+        Instruction::Cal(Operand::Literal(6.0)), // 3
+        Instruction::Sta(Operand::Literal(0.0), Operand::Register(Register::Rax)), // 4
+        Instruction::Hlt,                        // 5
         // スタックにある値を全部足す関数
-        Instruction::Pop(Register::Rdx), // 5
-        Instruction::Add(Register::Rax, Operand::Register(Register::Rdx)), // 6
+        Instruction::Pop(Register::Rdx), // 6
+        Instruction::Add(Register::Rax, Operand::Register(Register::Rdx)), // 7
         // スタックに値がある限り繰り返す
-        Instruction::Jmp(Operand::Register(Register::Rsp), Operand::Literal(5.0)), // 7
-        Instruction::Ret,                                                          // 8
+        Instruction::Jmp(Operand::Register(Register::Rsp), Operand::Literal(6.0)), // 8
+        Instruction::Ret,                                                          // 9
     ]);
     vm.run();
     vm.dump();
