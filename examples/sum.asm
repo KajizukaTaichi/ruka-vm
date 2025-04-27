c48dfb044d19d8fe24e8863f00d27bb94c674d92
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-27 10:44:52 +0900

    Update sum.asm

--- examples/sum.asm
+++ examples/sum.asm
@@ -3,20 +3,24 @@ main:
     psh 8
     psh 2
     cal sum     ; 合計する
-    pop rax     ; スタックから結果を取り出す
-    sta 0, rax  ; 結果をメモリに保存
+    pop ar      ; スタックから結果を取り出す
+    mov da, 0   ; 保存先アドレス: 0
+    sta da, ar  ; 結果をメモリに保存
     hlt         ; プログラムを終了する
+
+
 
 ; スタックにある値を合計する関数
 sum:
-    mov rax, 0          ; 合計値を初期化
+    mov ar, 0           ; 合計値を初期化
 loop_start:
-    mov rcx, rsp        ; スタックが空ならループ終了
-    nor rcx, rcx
-    jmp rcx, loop_end
-    pop rdx             ; スタックから値を取り出す
-    add rax, rdx        ; 加算する
+    mov cr, sp
+    nor cr, cr          ; スタックは空か？
+    jmp cr, loop_end    ; そうならループ終了
+loop_body:
+    pop dr              ; スタックから値を取り出す
+    add ar, dr          ; 加算する
     jmp 1, loop_start   ; ループ先頭に戻る
 loop_end:
-    psh rax             ; 合計値をスタックに積む
+    psh ar              ; 合計値をスタックに積む
     ret
