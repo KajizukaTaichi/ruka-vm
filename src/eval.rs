fed4979bcecd3837ad14e6fa1a57111fafd974b9
梶塚太智 <kajizukataichi@outlook.jp>
2025-04-28 14:29:39 +0900

    Revert "Update eval.rs"
    This reverts commit b3f27f836cf0208ba1d7479f7787603103ea4436.

--- src/eval.rs
+++ src/eval.rs
@@ -8,22 +8,22 @@ impl RukaVM {
         println!("{} Bytes free", size_of::<[f64; MEMORY_SIZE]>());
 
         RukaVM {
+            pc: 0.0,
             program,
             call: Vec::new(),
             stack: Vec::new(),
-            memory: [Value::new(0.0); MEMORY_SIZE],
-            pc: Value::new(0.0),
-            ar: Value::new(0.0),
-            dr: Value::new(0.0),
-            cr: Value::new(0.0),
-            ba: Value::new(0.0),
-            sp: Value::new(0.0),
+            memory: [0.0; MEMORY_SIZE],
+            ar: 0.0,
+            dr: 0.0,
+            cr: 0.0,
+            ba: 0.0,
+            sp: 0.0,
         }
     }
 
     pub fn run(&mut self) -> Option<()> {
         loop {
-            let instruction = self.program.get(self.pc.as_f64() as usize)?.clone();
+            let instruction = self.program.get(self.pc as usize)?.clone();
             match instruction {
                 Instruction::Mov(reg, val) => {
                     let val = self.get_operand(val);
@@ -33,45 +33,41 @@ impl RukaVM {
                 Instruction::Add(dst, src) => {
                     let operand = self.get_operand(src);
                     let reg = self.get_register(dst);
-                    reg.num = reg.den * operand.den + operand.num * reg.den;
-                    reg.den = reg.den * operand.den;
+                    *reg = *reg + operand
                 }
                 Instruction::Mul(dst, src) => {
                     let operand = self.get_operand(src);
                     let reg = self.get_register(dst);
-                    reg.den = reg.den * operand.num;
-                    reg.num = reg.num * operand.den;
+                    *reg = *reg * operand
                 }
                 Instruction::Neg(reg) => {
                     let reg = self.get_register(reg);
-                    reg.sign = !reg.sign
+                    *reg = -*reg
                 }
                 Instruction::Inv(reg) => {
                     let reg = self.get_register(reg);
-                    reg.num = reg.den;
-                    reg.den = reg.num;
+                    *reg = 1.0 / *reg;
                 }
                 Instruction::Eql(dst, src) => {
                     let operand = self.get_operand(src);
                     let reg = self.get_register(dst);
-                    *reg = Value::new(if *reg == operand { 1.0 } else { 0.0 })
+                    *reg = if *reg == operand { 1.0 } else { 0.0 }
                 }
                 Instruction::Les(dst, src) => {
                     let operand = self.get_operand(src);
                     let reg = self.get_register(dst);
-                    let result = reg.as_f64() < operand.as_f64();
-                    *reg = Value::new(if result { 1.0 } else { 0.0 })
+                    *reg = if *reg < operand { 1.0 } else { 0.0 }
                 }
                 Instruction::Nor(dst, src) => {
                     let operand = self.get_operand(src);
                     let reg = self.get_register(dst);
-                    let result = !(reg.num != 0 || operand.num != 0);
-                    *reg = Value::new(if result { 1.0 } else { 0.0 })
+                    let result = !(*reg != 0.0 || operand != 0.0);
+                    *reg = if result { 1.0 } else { 0.0 }
                 }
                 Instruction::Jmp(cond, addr) => {
                     let cond = self.get_operand(cond);
                     let addr = self.get_operand(addr);
-                    if cond.num != 0 {
+                    if cond != 0.0 {
                         self.pc = addr;
                         continue;
                     }
@@ -85,31 +81,31 @@ impl RukaVM {
                 Instruction::Ret => self.pc = self.call.pop()?,
                 Instruction::Lda(reg, addr) => {
                     let addr = self.get_operand(addr);
-                    let val = self.memory.get(addr.as_f64() as usize)?.clone();
+                    let val = self.memory.get(addr as usize)?.clone();
                     let reg = self.get_register(reg);
                     *reg = val
                 }
                 Instruction::Sta(addr, val) => {
                     let addr = self.get_operand(addr);
                     let val = self.get_operand(val);
-                    let addr = self.memory.get_mut(addr.as_f64() as usize)?;
+                    let addr = self.memory.get_mut(addr as usize)?;
                     *addr = val;
                 }
                 Instruction::Psh(val) => {
                     let val = self.get_operand(val);
                     self.stack.push(val);
-                    self.sp = Value::new(self.stack.len() as f64);
+                    self.sp += 1.0;
                 }
                 Instruction::Pop(reg) => {
                     let val = self.stack.pop()?;
                     let reg = self.get_register(reg);
                     *reg = val;
-                    self.sp = Value::new(self.stack.len() as f64);
+                    self.sp -= 1.0;
                 }
                 Instruction::Nop => {}
                 Instruction::Hlt => break,
             }
-            self.pc = Value::new(self.pc.as_f64() + 1.0);
+            self.pc += 1.0;
         }
         Some(())
     }
@@ -118,7 +114,7 @@ impl RukaVM {
         macro_rules! view {
             ($val: expr) => {{
                 let formatted = format!("{:08}", $val);
-                if $val.as_f64() != 0.0 {
+                if $val != 0.0 {
                     formatted.bold()
                 } else {
                     formatted.normal()
@@ -147,7 +143,7 @@ impl RukaVM {
         }
     }
 
-    fn get_register(&mut self, register: Register) -> &mut Value {
+    fn get_register(&mut self, register: Register) -> &mut f64 {
         match register {
             Register::Pc => &mut self.pc,
             Register::Ar => &mut self.ar,
@@ -158,7 +154,7 @@ impl RukaVM {
         }
     }
 
-    fn get_operand(&mut self, operand: Operand) -> Value {
+    fn get_operand(&mut self, operand: Operand) -> f64 {
         match operand {
             Operand::Literal(value) => value,
             Operand::Register(register) => self.get_register(register).clone(),
